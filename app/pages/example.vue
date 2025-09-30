89aeb8cfce76ae5287cf3cb9e2638ae0fb060229
blschill <93346436+blschill@users.noreply.github.com>
2025-09-30 11:50:46 -0700

    Start Example page

---
+++ app/pages/example.vue
@@ -0,0 +1,27 @@
+<!-- This is to give a place to demonstrate all components -->
+
+<template>
+    <FormBase :formData="formData" breadCrumbTitle="Example Form" 
+        :saveSuccess="true">
+        <FormGrid>
+            <FormTextInput label="Example Input" v-model="formData.text_1" 
+                placeholder="Enter example input" colWidth="quarter"/>
+            <FormTextInput label="Another Input" v-model="formData.text_2" 
+                placeholder="Enter another input" colWidth="quarter"/>
+            <FormComboBox label="Ex. Select (Users)" v-model="formData.combo_1" 
+                placeholder="Select an option" colWidth="half" 
+                search="users"/>
+        </FormGrid>
+    </FormBase>
+</template>
+
+<script setup lang="ts">
+const formData = ref<Record<string, any>>({
+    text_1: '',
+    text_2: '',
+    combo_1: {
+        id: null,
+        name: null,
+    },
+});
+</script>
